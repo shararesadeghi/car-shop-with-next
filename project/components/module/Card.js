f4e8565babe3b667aaba2ee103da72aabd4a93df
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-02 12:22:58 +0330

    card component created

---
+++ project/components/module/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Location from './../icons/Location';
+
+import styles from './Card.module.css';
+
+const Card = (props) => {
+
+    const{id, name, model, year, distance, location, image, price} = props;
+
+    return (
+        <div className={styles.container}>
+           <img src={image} className={styles.image} alt="car image"/> 
+           <h4 className={styles.title}>{`${name} ${model}`}</h4>
+           <p className={styles.detail}>{`${year} . ${distance}km`}</p>
+           <div className={styles.footer}>
+               <p className={styles.price}>$ {price}</p>
+               <div className={styles.location}>
+                   <p>{location}</p>
+                   <Location/>
+               </div>
+           </div>
+        </div>
+    );
+};
+
+export default Card;
