f69f18c35136a1a038f90ca79266eec3164c9132
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-02 16:13:14 +0330

    some state added to searchbar

--- project/components/module/SearchBar.js
+++ project/components/module/SearchBar.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './SearchBar.module.css';
 
 const SearchBar = () => {
+
+    const [min, setMin] = useState("");
+    const [max, setMax] =useState("");
+
+    const searchHandler
+
     return (
         <div className={styles.container}>
             <div>
-                <input placeholder="Enter min-price"/>
-                <input placeholder="Enter max-price"/>
+                <input placeholder="Enter min-price" vlaue={min} onChange={e=>setMin(e.target.value)}/>
+                <input placeholder="Enter max-price" value={max} onChange={e=>setMax(e.target.value)}/>
             </div>
-            <button>Search</button>
+            <button onClick={searchHandler}>Search</button>
         </div>
     );
 };
